24c5fef3394fa2a2eb13e08d67e53e2b86919c43
Jay Mark Estrera <Jay.Estrera@bcstechnology.com.au>
2019-03-24 15:16:35 +0800

    Created Service and Appointment.

---
+++ Pet911Website/Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pet911Website.Models
+{
+    public class Appointment
+    {
+        public Client Client { get; set; }
+        public Pet Pet { get; set; }
+        public Service Service { get; set; }
+        public DateTime? Schedule { get; set; }
+    }
+}
---
+++ Pet911Website/Models/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace Pet911Website.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
